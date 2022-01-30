852b071de20caa8b570fc3495098b62e7d416e06
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-01-30 08:23:23 -0800

    integer to roman.

---
+++ integer-to-roman.js
@@ -0,0 +1,61 @@
+/* Leetcode 20220130: https://leetcode.com/problems/integer-to-roman/
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+
+
+
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+Constraints:
+
+1 <= num <= 3999
+
+Runtime: 204 ms, faster than 47.62% of JavaScript online submissions for Integer to Roman.
+Memory Usage: 48.3 MB, less than 7.84% of JavaScript online submissions for Integer to Roman.
+*/
+const intToRoman = (num) => {
+    let x = num, s = '', dct = [
+    [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I'] ];
+    for (let i = 0; i < dct.length && x > 0; i++) {
+      let rep = Math.floor(x / dct[i][0]);
+      let tmp = '';
+      while (rep > 0) {
+        tmp = tmp + dct[i][1];
+        x = x - dct[i][0];
+        rep--;
+      }
+      s = s + tmp;
+    }
+    return s;
+  }
