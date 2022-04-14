939547cc946933380e059f4056232c9863c38f6c
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-04-14 09:55:04 -0700

    revisit twoSum.

--- scratch-20220412.js
+++ scratch-20220412.js
@@ -1,3 +1,4 @@
+/* 20220412
 function nthMax(arr, target) {
     if (target < 1 || target >= arr.length) return null;
     let sorted = arr.sort((a, b) => b - a);
@@ -16,7 +17,9 @@ function nthMax(arr, target) {
 let a = [1,2,3,2,5,6,7];
 let target = 7;
 console.log(nthMax(a, target))
-// FIND DUPLICATE CHARS AND AMT 10:18:46
+*/
+
+// FIND DUPLICATE CHARS AND AMT . 20220412. 10:18:46
 /*
 {'a':2, 'b':1, 'c': 1}
 {'a'
@@ -32,7 +35,7 @@ Plan
 - iterate through all key in resObj
   - check if val = 1 => delete that key
 - return resObj;
-*/
+
 function findDuplicate(str) {
     if (typeof str !== 'string') return false;
     let res = {};					// {'a':2}
@@ -46,14 +49,17 @@ function findDuplicate(str) {
     }
     return res;
   };
-
+*/
 //  console.log(findDuplicate('abca'))
 
-// REVERSE A STRING 06:53.16
+
+
+////////////////////////////////////////
+// REVERSE A STRING 20220412. 06:53.16
 /*'abc'
     i j
  'c'+'b'+'a' + ''
-*/
+*/ /*
 function reverse(str) {
     if (typeof str !== 'string') return false;	//
     let res = '';					// res = 'cba'
@@ -63,6 +69,7 @@ function reverse(str) {
     }
     return res;
   }
+*/
 
   /*
    012 = 3
@@ -70,5 +77,49 @@ function reverse(str) {
       i
   */
 
+
+// 20220413-1. 04:52:59
+// function reverse(str) {
+//     if (typeof str !== 'string') return false;
+//     let res = '';					// res = 'cba'
+//     for (let i = str.length - 1; i >= 0; i--) {	// i = 0 >= 0 T
+//       let c = str[i];				// c = 'a'
+//       res = res + c;
+//     }
+//     return res;
+//   }
+  /*
+
+   0
+   012 = 3
+  'abc'
+  */
+
 // let s = 'abc'
 // console.log(reverse(s))
+
+// 20220414. 11:00.42
+// Move negatives to the front of list
+// function moveNeg(nums) {
+//     let len = nums.length;
+//     for (let i = 0; i < len; i++) {
+//         let n = nums[i];
+//         if (n > 0) {
+//             let spliced = nums.splice(i, 1)[0];
+//             nums.push(spliced);
+//             i--;
+//             len--;
+//         }
+//     }
+//     return nums;
+// }
+
+// let nums = [1,4,3,-2,-1,1,5,3,-2,1,6,-1,4];
+// console.log(moveNeg(nums))
+
+class Name {
+    constructor(x, y) {
+        thix.x = x;
+        this.y = y;
+    }
+}
